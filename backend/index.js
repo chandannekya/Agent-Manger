1a6fa1515fe24f9d95a435f0472683b55b47ebed
Chandan <chadan85338818@gmail.com>
2025-03-15 07:50:44 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://agent-manger.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
 
     allowedHeaders: ["Content-Type", "Authorization"],
